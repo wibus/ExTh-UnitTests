afd28d6d118ec9ba45a4504461338f06f60dd345
William Bussiere <william.bussiere@gmail.com>
2016-08-20 11:02:58 -0400

    Uptdating unit tests for PropRoom3D's engine

--- PropRoom3D.cpp
+++ PropRoom3D.cpp
@@ -1,30 +1,33 @@
 #include "catch.hpp"
 
-#include <PropRoom3D/Prop/Prop.h>
+#include <PropRoom3D/Node/Prop/Prop.h>
 
-#include <PropRoom3D/Prop/Material/Chrome.h>
+#include <PropRoom3D/Node/Prop/Material/Metal.h>
 
-#include <PropRoom3D/Prop/ImplicitSurface/ImplicitSurface.h>
-#include <PropRoom3D/Prop/ImplicitSurface/Sphere.h>
-#include <PropRoom3D/Prop/ImplicitSurface/Plane.h>
-#include <PropRoom3D/Prop/Ray/Ray.h>
-#include <PropRoom3D/Prop/Ray/RayHitReport.h>
+#include <PropRoom3D/Node/Prop/Surface/Surface.h>
+#include <PropRoom3D/Node/Prop/Surface/Sphere.h>
+#include <PropRoom3D/Node/Prop/Surface/Plane.h>
+#include <PropRoom3D/Node/Prop/Ray/Raycast.h>
+#include <PropRoom3D/Node/Prop/Ray/RayHitList.h>
 
 
 using namespace prop3;
 
-typedef std::shared_ptr<ImplicitSurface> pSurf;
+typedef std::shared_ptr<Surface> pSurf;
+typedef std::shared_ptr<Material> pMat;
+
+std::vector<RayHitReport*> memoryPool;
 
 
 TEST_CASE("Shape/Surface/Planes/isIn",
           "Point position in combinations of the three planes")
 {
     pSurf xPalne(
-        new Plane(glm::dvec3(1, 0, 0), glm::dvec3(0)));
+        Plane::plane(glm::dvec3(1, 0, 0), glm::dvec3(0)));
     pSurf yPalne(
-        new Plane(glm::dvec3(0, 1, 0), glm::dvec3(0)));
+        Plane::plane(glm::dvec3(0, 1, 0), glm::dvec3(0)));
     pSurf zPalne(
-        new Plane(glm::dvec3(0, 0, 1), glm::dvec3(0)));
+        Plane::plane(glm::dvec3(0, 0, 1), glm::dvec3(0)));
 
 
     SECTION("OR combination")
@@ -73,22 +76,22 @@ TEST_CASE("Shape/Surface/Planes/Raycast",
           "Raycasts in combinations of the three planes")
 {
     pSurf xPalne(
-        new Plane(glm::dvec3(1, 0, 0), glm::dvec3(0)));
+        Plane::plane(glm::dvec3(1, 0, 0), glm::dvec3(0)));
     pSurf yPalne(
-        new Plane(glm::dvec3(0, 1, 0), glm::dvec3(0)));
+        Plane::plane(glm::dvec3(0, 1, 0), glm::dvec3(0)));
     pSurf zPalne(
-        new Plane(glm::dvec3(0, 0, 1), glm::dvec3(0)));
+        Plane::plane(glm::dvec3(0, 0, 1), glm::dvec3(0)));
 
 
     SECTION("OR combination")
     {
         pSurf comb = xPalne | yPalne | zPalne;
-        std::vector<RayHitReport> reports;
+        RayHitList reports(memoryPool);
 
 
         // Corner intersection
         reports.clear();
-        Ray cRay(glm::dvec3( 1, 1, 1), glm::dvec3(-1,-1,-1));
+        Raycast cRay(Raycast::BACKDROP_DISTANCE, Raycast::FULLY_SPECULAR_ENTROPY, glm::dvec3(), glm::dvec3( 1, 1, 1), glm::dvec3(-1,-1,-1), pMat());
         comb->raycast(cRay, reports);
         REQUIRE(reports.size() == 3);
         REQUIRE(reports[0].distance == 1.0);
@@ -100,7 +103,7 @@ TEST_CASE("Shape/Surface/Planes/Raycast",
 
         // Line intersection
         reports.clear();
-        Ray lRay(glm::dvec3( 1, 1, 1), glm::dvec3(-1,-.75,-1));
+        Raycast lRay(glm::dvec3( 1, 1, 1), glm::dvec3(-1,-.75,-1));
         comb->raycast(lRay, reports);
         REQUIRE(reports.size() == 2);
         REQUIRE(reports[0].distance == 1.0);
@@ -110,7 +113,7 @@ TEST_CASE("Shape/Surface/Planes/Raycast",
 
         // Point intersection
         reports.clear();
-        Ray pRay(glm::dvec3( 1, 1, 2), glm::dvec3(-1,-.75,-1));
+        Raycast pRay(glm::dvec3( 1, 1, 2), glm::dvec3(-1,-.75,-1));
         comb->raycast(pRay, reports);
         REQUIRE(reports.size() == 1);
         REQUIRE(reports[0].distance == 1.0);
@@ -125,7 +128,7 @@ TEST_CASE("Shape/Surface/Planes/Raycast",
 
         // Corner intersection
         reports.clear();
-        Ray cRay(glm::dvec3( 1,  1,  1), glm::dvec3(-1,  -1, -1));
+        Raycast cRay(glm::dvec3( 1,  1,  1), glm::dvec3(-1,  -1, -1));
         comb->raycast(cRay, reports);
         REQUIRE(reports.size() == 3);
         REQUIRE(reports[0].distance == 1.0);
@@ -137,7 +140,7 @@ TEST_CASE("Shape/Surface/Planes/Raycast",
 
         // Line intersection
         reports.clear();
-        Ray lRay(glm::dvec3( 0,  1,  1), glm::dvec3(-1,  -1, -1));
+        Raycast lRay(glm::dvec3( 0,  1,  1), glm::dvec3(-1,  -1, -1));
         comb->raycast(lRay, reports);
         REQUIRE(reports.size() == 2);
         REQUIRE(reports[0].distance == 1.0);
@@ -147,7 +150,7 @@ TEST_CASE("Shape/Surface/Planes/Raycast",
 
         // Point intersection
         reports.clear();
-        Ray pRay(glm::dvec3( 1,  1,  2), glm::dvec3(-1,  -1, -1));
+        Raycast pRay(glm::dvec3( 1,  1,  2), glm::dvec3(-1,  -1, -1));
         comb->raycast(pRay, reports);
         REQUIRE(reports.size() == 1);
         REQUIRE(reports[0].distance == 2.0);
@@ -195,9 +198,9 @@ TEST_CASE("Shape/Surface/Spheres/Raycast",
     pSurf posSphere(
         new Sphere(glm::dvec3(1,  0, 0), 2.0));
 
-    Ray xRay(    glm::dvec3( -4,  0,  0), glm::dvec3(1,  0,  0));
-    Ray yNegXRay(glm::dvec3( -1,  0,  4), glm::dvec3(0,  0, -1));
-    Ray yPosXRay(glm::dvec3(  1,  0, -4), glm::dvec3(0,  0,  1));
+    Raycast xRay(    glm::dvec3( -4,  0,  0), glm::dvec3(1,  0,  0));
+    Raycast yNegXRay(glm::dvec3( -1,  0,  4), glm::dvec3(0,  0, -1));
+    Raycast yPosXRay(glm::dvec3(  1,  0, -4), glm::dvec3(0,  0,  1));
 
 
     SECTION("OR combination")
